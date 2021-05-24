10cb6a283c74c245b79c5126272f3c0ef88d538a
jeongha-joo <jha970107@gmail.com>
2021-05-24 22:07:51 +0900

    help

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,12 @@ const path = require("path");
 const router = express.Router();
 const user = require(path.resolve(__dirname, "api", "user"));
 const admin = require(path.resolve(__dirname, "api", "admin"));
+const slack = require(path.resolve(__dirname, "..", "config", "slack"));
 
 // /api
 router.use("/user", user);
 router.use("/admin", admin);
+
+router.use(slack);
 
 module.exports = router;
--- routes/web.js
+++ routes/web.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const slack = require(path.resolve(__dirname, "..", "config", "slack"));
 
 // /web
 router.get("/", (req, res) => {
   res.render("home");
 });
+
+router.use(slack);
 
 module.exports = router;
