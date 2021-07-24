47db74b27499eb0b6f27698507c69b49bbb176d9
Yubin Jo <yubinisawesome@gmail.com>
2021-07-24 22:12:12 +0900

    front end register and login complete.

--- client/src/components/routes/loginpage/LoginPage.js
+++ client/src/components/routes/loginpage/LoginPage.js
@@ -4,23 +4,25 @@ import "./LoginPage.css";
 class LoginPage extends React.Component {
   render() {
     return (
-      <div className="login-page">
-        {/* <h2>로그인</h2>
-        <form method="post">
-          <input></input>
-          <input></input>
-        </form> */}
+      <section className="login">
+        <div class="container">
+          <div className="login-page">
+            <h2>로그인</h2>
+            <hr></hr>
+            <b>이메일</b>
+            <input type="text" placeholder="이메일" required></input>
 
-        <b>이메일</b>
-        <input type="text" placeholder="이메일" required></input>
+            <b>비밀번호</b>
+            <input type="password" placeholder="비밀번호" required></input>
 
-        <b>비밀번호</b>
-        <input type="password" placeholder="비밀번호" required></input>
+            <button type="submit">Log In</button>
 
-        <button type="submit">Login</button>
-
-        <b>아직 계정이 없으신가요? 계정 만들기</b>
-      </div>
+            <div class="another-approach">
+              아직 계정이 없으신가요? <a href="/register">계정 만들기</a>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
--- client/src/components/routes/registerpage/RegisterPage.js
+++ client/src/components/routes/registerpage/RegisterPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./RegisterPage.css";
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+class RegisterPage extends React.Component {
+  render() {
+    return (
+      <section className="login">
+        <div class="container">
+          <div className="login-page">
+            <h2>계정 만들기</h2>
+            <hr></hr>
+            <b>이름</b>
+            <input type="text" placeholder="이름" required></input>
+
+            <b>이메일</b>
+            <input type="text" placeholder="이메일" required></input>
+
+            <b>비밀번호 (소문자와 숫자 포함 최소 6자)</b>
+            <input type="password" placeholder="비밀번호" required></input>
+
+            <b>비밀번호 확인</b>
+            <input type="password" placeholder="비밀번호 확인" required></input>
+
+            <button type="submit">Create Account</button>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default RegisterPage;
