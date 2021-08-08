0f153bee8d4bf6c6d2e5bef28abe5b31886240a6
Yubin Jo <yubinisawesome@gmail.com>
2021-08-09 02:54:22 +0900

    returnbox api 주소 개선

--- routes/api/returnbox.js
+++ routes/api/returnbox.js
@@ -3,51 +3,50 @@ const express = require("express");
 const path = require("path");
 const controllerPath = path.resolve(__dirname, "..", "..", "controller");
 
-
 // path variable
 const returnBoxInsert = require(path.resolve(
-    // folder name
-    controllerPath,
-    // folder name
-    "returnbox",
-    // file name
-    "ReturnBoxInsert"
+  // folder name
+  controllerPath,
+  // folder name
+  "returnbox",
+  // file name
+  "ReturnBoxInsert"
 ));
 
 const returnBoxSelect = require(path.resolve(
-    // folder name
-    controllerPath,
-    // folder name
-    "returnbox",
-    // file name
-    "ReturnBoxSelect"
+  // folder name
+  controllerPath,
+  // folder name
+  "returnbox",
+  // file name
+  "ReturnBoxSelect"
 ));
 
 const returnBoxUpdate = require(path.resolve(
-    // folder name
-    controllerPath,
-    // folder name
-    "returnbox",
-    // file name
-    "ReturnBoxUpdate"
+  // folder name
+  controllerPath,
+  // folder name
+  "returnbox",
+  // file name
+  "ReturnBoxUpdate"
 ));
 
 const returnBoxDelete = require(path.resolve(
-    // folder name
-    controllerPath,
-    // folder name
-    "returnbox",
-    // file name
-    "ReturnBoxDelete"
+  // folder name
+  controllerPath,
+  // folder name
+  "returnbox",
+  // file name
+  "ReturnBoxDelete"
 ));
 
 // move to controller
 const router = express.Router();
 
-router.post("/insert", returnBoxInsert);
-router.get("/find/:_id", returnBoxSelect);
-router.put("/update/:_id", returnBoxUpdate);
-router.delete("/delete/:_id", returnBoxDelete);
+router.post("/", returnBoxInsert);
+router.get("/:_id", returnBoxSelect);
+router.put("/:_id", returnBoxUpdate);
+router.delete("/:_id", returnBoxDelete);
 
 // creating a module for usage in other files
 module.exports = router;
